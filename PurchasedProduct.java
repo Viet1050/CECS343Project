813da7c8c4944b2cdbb8105c88161cd96760bf46
Viet Nguyen <50393098+Viet1050@users.noreply.github.com>
2019-11-08 09:18:01 -0800

    Add files via upload

--- PurchasedProduct.java
+++ PurchasedProduct.java
@@ -3,4 +3,20 @@ package test;
 public class PurchasedProduct {
 	private String productName;
 	private int quantity;
+	
+	public PurchasedProduct(String name, int quant) {
+		productName = name;
+		quantity = quant;
+	}
+	
+	public String getName() {
+		return productName;
+	}
+	public int getQuanitity() {
+		return quantity;
+	}
+	public void setQuantity(int quant) {
+		quantity = quant;
+		
+	}
 }
